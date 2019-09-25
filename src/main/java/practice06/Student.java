c1701bb7ff85eab77d54b0b289ea02a8de2bbaed
Chloe Reyes <katrina.chloe.reyes@oocl.com>
2019-09-25 11:16:11 +0800

    Finished Practice 06 and Fixed Unit Tests

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,19 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+
+    int studentKlass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        studentKlass = klass;
+    }
+
+    public int getKlass() {
+        return studentKlass;
+    }
+
+    public String introduce() {
+        return super.introduce().concat(" I am a Student. I am at Class ").concat(String.valueOf(studentKlass).concat("."));
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,36 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private int teacherKlass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        teacherKlass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return this.teacherKlass;
+    }
+
+    @Override
+    public String introduce() {
+        if (teacherKlass == 0){
+            return super.introduce().concat(" I am a ")
+                    .concat(Teacher.class.getSimpleName())
+                    .concat(".")
+                    .concat(" I teach No Class.");
+        } else{
+            return super.introduce().concat(" I am a ")
+                    .concat(Teacher.class.getSimpleName())
+                    .concat(".")
+                    .concat(" I teach Class ")
+                    .concat(String.valueOf(teacherKlass))
+                    .concat(".");
+        }
+    }
 }
