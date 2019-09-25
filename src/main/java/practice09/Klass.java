8c17deedeb072466d2684a1c0f9eb6811759a073
Chloe Reyes <katrina.chloe.reyes@oocl.com>
2019-09-25 11:24:54 +0800

    Finished Practice 09 and Fixed Unit Tests

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,40 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+
 public class Klass {
+    private int number;
+    private Student student;
+    private List<String> students = new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return format("Class %d", number);
+    }
+
+    public void assignLeader(Student student) {
+        if (students.contains(student.getName())) {
+            this.student = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return student;
+    }
+
+    public void appendMember(Student student) {
+        this.students.add(student.getName());
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,35 @@
 package practice09;
 
-public class Teacher {
+import static java.lang.String.format;
+
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id,name,age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (klass == null){
+            return super.introduce() + format(" I am a Teacher. I teach No Class.");
+        }
+        return super.introduce() +
+                format(" I am a Teacher. I teach Class %d.", klass.getNumber());
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass() == this.getKlass()){
+            return super.introduce() + format(" I am a Teacher. I teach %s.", student.getName());
+        }
+        return super.introduce() + format(" I am a Teacher. I don't teach %s.", student.getName());
+    }
 }
