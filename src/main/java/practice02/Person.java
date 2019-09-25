df6a088cdc05e37d33b7caaebacb8e5208ae869e
Chloe Reyes <katrina.chloe.reyes@oocl.com>
2019-09-25 11:04:36 +0800

    Finished Practice 02 and Fixed Unit Tests

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,25 @@
 package practice02;
 
-public class Person {
+public class Person{
+
+    private final int personAge;
+    private final String personName;
+
+    public Person(String name, int age) {
+        personName = name;
+        personAge = age;
+    }
+
+    public String getName() {
+        return personName;
+    }
+
+    public int getAge() {
+        return personAge;
+    }
+
+    public String introduce() {
+        return "My name is ".concat(personName).concat(". I am ")
+                .concat(String.valueOf(personAge)).concat(" years old.");
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,19 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+
+    int studentKlass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        studentKlass = klass;
+    }
+
+    public int getKlass() {
+        return studentKlass;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class ".concat(String.valueOf(studentKlass).concat("."));
+    }
 }
