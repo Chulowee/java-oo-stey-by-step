55417404f19e6f09e63f73f6100b5ab952e3218d
Chloe Reyes <katrina.chloe.reyes@oocl.com>
2019-09-25 11:11:05 +0800

    Finished Practice 04 and Fixed Unit Tests

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,29 @@
 package practice04;
 
 public class Person {
+
+    private final int personAge;
+    private final String personName;
+
+    public Person(String name, int age) {
+        personName = name;
+        personAge = age;
+    }
+
+    public String getName() {
+        return personName;
+    }
+
+    public int getAge() {
+        return personAge;
+    }
+
+    public String introduce() {
+        return backIntroduce();
+    }
+
+    public String backIntroduce() {
+        return "My name is ".concat(personName).concat(". I am ")
+                .concat(String.valueOf(personAge)).concat(" years old.");
+    }
 }
